917f97af37af530fd78f1f119993152c7727b630
2610chuck-ai <2610chuck@gmail.com>
2026-05-20 15:49:33 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.APP_CONFIG = {
-  companyName: 'Kebap-Bestellung Donnerstag',
-  whatsappNumber: '',
-  whatsappDefaultCountryCode: '49',
-  apiBase: '/.netlify/functions',
-  autoRefreshMs: 15000,
-  demoMode: false
-};
